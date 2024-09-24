90c7a8c8fa6784066114113eef59af15e690d141
Simon Højberg <r.hackr@gmail.com>
2024-09-24 13:44:27 -0400

    Add initial Tauri app and LICENSE

---
+++ src/Main.elm
@@ -0,0 +1,103 @@
+module Main exposing (..)
+
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (Html, article, h1, main_, text)
+import Html.Attributes exposing (class)
+import Url exposing (Url)
+
+
+type Screen
+    = HomeScreen
+
+
+type alias Model =
+    { screen : Screen
+    }
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( { screen = HomeScreen
+      }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | UrlRequest Browser.UrlRequest
+    | UrlChange Url
+    | ChangeScreen Screen
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ChangeScreen s ->
+            ( { model | screen = s }, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+viewScreenContent : Screen -> Html Msg
+viewScreenContent screen =
+    let
+        title t =
+            h1 [] [ text t ]
+
+        ( title_, content ) =
+            case screen of
+                HomeScreen ->
+                    ( title "UCM", text "Hello World" )
+    in
+    article [ class "screen-content" ] [ title_, content ]
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "UCM"
+    , body =
+        [ main_ []
+            [ viewScreenContent model.screen
+            ]
+        ]
+    }
+
+
+
+-- PROGRAM
+
+
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        , onUrlRequest = UrlRequest
+        , onUrlChange = UrlChange
+        }
