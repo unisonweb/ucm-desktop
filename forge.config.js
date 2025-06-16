03bbcb3ae1edddc98c02b4fb2fa922a8c6bfd65a
Simon Højberg <r.hackr@gmail.com>
2025-06-16 16:11:53 -0400

    Update publish config to overwrite release

--- forge.config.js
+++ forge.config.js
@@ -102,7 +102,8 @@ module.exports = {
           owner: 'unisonweb',
           name: 'ucm-desktop'
         },
-        prerelease: true
+        prerelease: true,
+        force: true
       }
     }
   ]
