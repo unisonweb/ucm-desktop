509427cb44a20605bd08cab9e6a0c3172c057355
Simon Højberg <r.hackr@gmail.com>
2025-07-02 15:37:09 -0400

    Improve linux traffic controls sizing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { app, BrowserWindow } from 'electron';
-import path from 'node:path';
 import started from 'electron-squirrel-startup';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
